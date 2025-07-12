c24797490f78403ad6afc56848cda81b7bc00d1a
axcids <axcids@gmail.com>
2025-07-12 23:27:19 +0300

    update

--- Spareparts.Application/ProductsDetails/Commands/DeleteProductDetailsById/DeleteProductDetailsByIdCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-
-namespace Spareparts.Application.ProductsDetails.Commands.DeleteProductDetailsById; 
-public class DeleteProductDetailsByIdCommand (Guid id) : IRequest<bool>{
-    public Guid Id { get; } = id;
-}
--- Spareparts.Application/ProductsDetails/Queries/GetAllProductsDetails/GetAllProductsDetailsQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-using Spareparts.Application.ProductsDetails.Dtos;
-
-namespace Spareparts.Application.ProductsDetails.Queries.GetAllProductsDetails; 
-public class GetAllProductsDetailsQuery : IRequest<IEnumerable<ProductsDetailsDtos>>{}
